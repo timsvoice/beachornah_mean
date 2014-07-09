affdc5cd0d0850423d119482cd864133c28841f1
timsvoice <timsethvoice@gmail.com>
2014-07-09 15:17:33 -0400

    closer

--- source/javascripts/mainController.js
+++ source/javascripts/mainController.js
@@ -1,17 +1,32 @@
-app.controller('mainController', function($scope, $http){
-    $scope.apiKey = "b8b0ce91aff80e7fc445db42a798738b16a956ed";
-    $scope.results = [];
-    $scope.init = function() {
-        //API requires location
-        var location = "40.560624,-73.870182";
-        $http.get('http://api.worldweatheronline.com/free/v1/marine.ashx?format=json&q=' + location + '&key=' + $scope.apiKey).success(function(data) {
-                
-            angular.forEach(data.data.weather[ 0 ].hourly, function(stats, index){
-              $scope.results.push(stats);
-            });
-            console.log($scope.results);
-        }).error(function(error) {
- 
-        });
-    };
-});
+app.controller('mainController', ['$scope', 'weatherData', function($scope, weatherData) {
+
+  var results = weatherData;
+
+  $scope.results = results;
+  
+  console.log(results);
+
+}]);
+
+app.factory('weatherData', function($http) {
+
+      //API requires location
+
+      var results = [];
+
+      var location = "40.560624,-73.870182";
+      var apiKey = "b8b0ce91aff80e7fc445db42a798738b16a956ed";
+
+      $http.get('http://api.worldweatheronline.com/free/v1/marine.ashx?format=json&q=' + location + '&key=' + $scope.apiKey).success(function(data) {
+              
+          angular.forEach(data.data.weather[ 0 ].hourly, function(stats, index){
+            results.push(stats);
+          });
+          console.log(results);
+      }).error(function(error) {
+
+      });
+    
+    return results;
+
+  });
--- source/javascripts/services.js
+++ source/javascripts/services.js
@@ -1,15 +1,22 @@
-app.factory('weatherAPI', function($http) {
+app.factory('WeatherData', function($http) {
 
-    var location = "40.560624,-73.870182";
-    var apiKey = "b8b0ce91aff80e7fc445db42a798738b16a956ed";
-    
+    var results = [];
 
-     $scope.init = function() {
-        //Create the date string and ensure leading zeros if required
-        $http.get('http://api.worldweatheronline.com/free/v1/marine.ashx?format=json&q=' + location + '&key=' + apiKey).success(function(data) {
-            console.log(data);
-        }).error(function(error) {
- 
-        });
+    this.initialize = function() {
+      //API requires location
+      
+      var location = "40.560624,-73.870182";
+      var apiKey = "b8b0ce91aff80e7fc445db42a798738b16a956ed";
+
+      $http.get('http://api.worldweatheronline.com/free/v1/marine.ashx?format=json&q=' + location + '&key=' + $scope.apiKey).success(function(data) {
+              
+          angular.forEach(data.data.weather[ 0 ].hourly, function(stats, index){
+            results.push(stats);
+          });
+          console.log(results);
+      }).error(function(error) {
+
+      });
     };
+
   });
