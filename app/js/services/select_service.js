228ccb0ae3affcde7628dd2e32f0812705243fb9
timothy voice <timothyvoice@tm-110722.tsao-mckown.com>
2015-05-04 18:36:15 -0400

    clear folder

--- app/js/services/select_service.js
+++
@@ -1,7 +0,0 @@
-app.factory('selectService', function() {
-    $scope.user = {};
-
-    $scope.update = function(user) {
-        $scope.user = angular.copy(user);
-    };
-});
--- generated/angular/template-cache.js
+++
@@ -1,3 +0,0 @@
-angular.module("beachornah").run(["$templateCache", function($templateCache) {
-""
-}]);
